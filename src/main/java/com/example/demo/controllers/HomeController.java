f66d9243ec6110babe9485fc6b6b6c4510296d52
katkola <katkola@gmail.com>
2022-12-16 20:27:49 -0500

    Changes to be committed: 	modified:   src/main/java/com/example/demo/controllers/HomeController.java

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,5 +1,24 @@
 package com.example.demo.controllers;
 
-public class HomeController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class HomeController {
+	
+	
+	@RequestMapping("/")
+    public String index(
+    		@RequestParam(value="name", required=false, defaultValue="human") String name,
+    		@RequestParam(value="last_name", required=false, defaultValue=" ") String last_name,
+    		@RequestParam(value="time", required=false, defaultValue="1") String time) {
+		int times = Integer.parseInt(time);
+		String output = "";
+		for(int i=0;i<times; i++) {
+			output+= String.format("Hello %s %s ", name, last_name);
+		}
+		
+        return output;
+    }
 }
